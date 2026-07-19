36798bb3f1594bc4ba929889b990dcb05d222861
desiato <hbdesiato@gmx.de>
2026-07-19 18:06:34 +0200

    make podman image inspect Digest deterministic

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 . ./env.sh
 
-podman pull --arch=amd64 "$BASE_IMAGE"
+podman pull  "$BASE_IMAGE"
 podman build \
     --security-opt=label=disable \
     --build-arg "BASE_IMAGE=$BASE_IMAGE" \
--- check.sh
+++ check.sh
@@ -6,11 +6,11 @@ pkg-list() {
     podman run --rm "$1" rpm -q --qf="%{NAME}\n" -a
 }
 
-podman pull --arch=amd64 "${BASE_IMAGE}"
+podman pull  "${BASE_IMAGE}"
 DIFF=$(printf '%s\n%s\n' "$(pkg-list "$BASE_IMAGE")" "$(pkg-list "$IMAGE")" | sort | uniq -u)
 set -x
 podman run --rm "$IMAGE" dnf check-upgrade $DIFF
 
 DIGEST1="$(podman image inspect --format '{{index .Annotations "org.opencontainers.image.base.digest"}}' "${IMAGE}")"
-DIGEST2="$(podman image inspect --format '{{.Digest}}' "${BASE_IMAGE}")"
+DIGEST2="$(skopeo inspect containers-storage:${BASE_IMAGE} --format '{{.Digest}}')"
 [ "${DIGEST1}" = "${DIGEST2}" ]
