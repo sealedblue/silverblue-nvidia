d9e7d293a46f52a25de2042f0a8efb8a243e0b5c
desiato <hbdesiato@gmx.de>
2026-07-25 20:52:34 +0200

    skip testing branch build if not diverged

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 set -euxo pipefail
 . ./env.sh
 
-podman pull  "$BASE_IMAGE"
-podman build \
-    --security-opt=label=disable \
-    --build-arg "BASE_IMAGE=$BASE_IMAGE" \
-    -t "${IMAGE}" .
+git remote set-head origin -a
+MAIN_BRANCH="$(git symbolic-ref refs/remotes/origin/HEAD | cut -d/ -f4)"
+if [[ "$TAG" != "$MAIN_BRANCH" ]] && ! [[ -e "diverge-$TAG" ]]; then
+    podman tag "${IMAGE_PREFIX}/${IMAGE_NAME}:${MAIN_BRANCH}-unsealed" "${IMAGE}"
+else
+    podman pull  "$BASE_IMAGE"
+    podman build \
+        --security-opt=label=disable \
+        --build-arg "BASE_IMAGE=$BASE_IMAGE" \
+        -t "${IMAGE}" .
+fi
 ./push.sh
