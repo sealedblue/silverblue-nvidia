71836d44ea1d397ab7dd9481b6f5dbf2560ad46d
desiato <hbdesiato@gmx.de>
2026-07-25 21:47:47 +0200

    fix image not known

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 . ./env.sh
 
 if [[ "$TAG" != "$MAIN_BRANCH" ]] && ! [[ -e "diverge-$TAG" ]]; then
+    podman pull  "$MAIN_IMAGE"
     podman tag "$MAIN_IMAGE" "${IMAGE}"
 else
     podman pull  "$BASE_IMAGE"
