21a0d8a87e9b6ef2fcc5df6b3be083f8d54d45e8
desiato <hbdesiato@gmx.de>
2026-07-25 22:14:23 +0200

    localhost:5000/silverblue-nvidia:main-unsealed pushed

--- build.sh
+++ build.sh
@@ -3,13 +3,16 @@ set -euxo pipefail
 . ./env.sh
 
 if [[ "$TAG" != "$MAIN_BRANCH" ]] && ! [[ -e "diverge-$TAG" ]]; then
-    podman pull  "$MAIN_IMAGE"
-    podman tag "$MAIN_IMAGE" "${IMAGE}"
+    DIGEST_NAME=$(systemd-escape "$IMAGE")
+    skopeo copy --sign-by-sigstore-private-key keys/sealedblue-staged.private \
+        --sign-passphrase-file keys/sealedblue-staged.passphrase \
+        --digestfile "${DIGEST_NAME}.digest" \
+        "docker://${MAIN_IMAGE}" "docker://${IMAGE}"
 else
     podman pull  "$BASE_IMAGE"
     podman build \
         --security-opt=label=disable \
         --build-arg "BASE_IMAGE=$BASE_IMAGE" \
         -t "${IMAGE}" .
+    ./push.sh
 fi
-./push.sh
